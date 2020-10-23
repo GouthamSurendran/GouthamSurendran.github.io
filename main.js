e94b861ebea0cdab86f8057690c7a9ac196d0fc0
GouthamSurendran <gouthu.g3@gmail.com>
2020-10-23 12:16:28 +0530

    add main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+function toggle() {
+    var x = document.getElementById("myTopNav");
+    if (x.className === "navBar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navBar";
+    }
+}
+var prevPos = window.pageYOffset;
+window.onscroll = function () {
+    var currPos = window.pageYOffset;
+    if (currPos > prevPos) {
+        document.getElementById("myTopNav").style.display = "none";
+    }
+    else {
+        var x = document.getElementById("myTopNav");
+        if (x.className == 'navBar responsive')
+            x.className = 'navBar';
+        document.getElementById("myTopNav").style.display = "initial";
+    }
+    prevPos = currPos;
+}
